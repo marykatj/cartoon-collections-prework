b19dda11324046ef2ba37b6ce27a3e71cfcdd2b4
Mary-Kate James <marykatj@gmail.com>
2018-05-03 18:48:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,11 @@ def summon_captain_planet(planeteer_calls)
   return planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+  if call.length > 4
+    return true
+  else return false
 end
 
 def find_the_cheese# code an argument here
