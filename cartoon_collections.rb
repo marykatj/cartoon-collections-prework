aa33f8b49d0646c091ec96a8643998a965e84663
Mary-Kate James <marykatj@gmail.com>
2018-05-03 18:39:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| puts call.capitalize + '!' }
+  planeteer_calls.map do |call| 
+  puts call.capitalize + '!'
+end
   return planeteer_calls
 end
 
