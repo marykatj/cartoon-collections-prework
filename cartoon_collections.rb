6d0f02c41bba0471f6c2168af91548e1b6740653
Mary-Kate James <marykatj@gmail.com>
2018-05-03 18:32:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.join(" ")
-  planeteer_calls.collect { |call| call.capitalize.split(" ")}
+  planeteer_calls.collect { |call| call.capitalize.split}
 end
 
 def long_planeteer_calls# code an argument here
