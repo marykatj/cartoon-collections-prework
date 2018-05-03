69d068f035b175d4361b6e74d7df51d92d8a0cd7
Mary-Kate James <marykatj@gmail.com>
2018-05-03 18:31:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.join.collect(" , ") { |call| call.capitalize.split(" ")}
+  planeteer_calls.join(" ")
+  planeteer_calls.collect { |call| call.capitalize.split(" ")}
 end
 
 def long_planeteer_calls# code an argument here
